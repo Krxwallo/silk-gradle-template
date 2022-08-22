d17b18fc0a615fa0ebbdaa39906702755a6133ba
Krxwallo <59575572+Krxwallo@users.noreply.github.com>
2022-08-22 19:09:19 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+val javaVersion = 17
+val silkVersion = "1.9.1"
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("fabric-loom") version "0.12-SNAPSHOT"
+}
+
+group = "org.example"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:1.19.2")
+    mappings("net.fabricmc:yarn:1.19.2+build.8")
+    modImplementation("net.fabricmc:fabric-loader:0.14.9")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.60.0+1.19.2")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.8.2+kotlin.1.7.10")
+
+    modImplementation("net.silkmc:silk-core:$silkVersion")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjdk-release=$javaVersion")
+            jvmTarget = "$javaVersion"
+        }
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(javaVersion)
+    }
+    processResources {
+        val properties = mapOf("version" to project.version)
+        inputs.properties(properties)
+        filesMatching("fabric.mod.json") { expand(properties) }
+    }
+}
