8d7aba88a6722b68377993195f8fad58edceb163
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-06-20 15:23:44 +0200

    kotlin 1.8.22, mc 1.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 val javaVersion = 17
-val silkVersion = "1.9.8"
+val silkVersion = "1.10.0"
 
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.8.22"
     id("fabric-loom") version "1.1-SNAPSHOT"
 }
 
@@ -14,11 +14,11 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.19.4")
-    mappings("net.fabricmc:yarn:1.19.4+build.2")
-    modImplementation("net.fabricmc:fabric-loader:0.14.19")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.78.0+1.19.4")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.9.3+kotlin.1.8.20")
+    minecraft("com.mojang:minecraft:1.20.1")
+    mappings("net.fabricmc:yarn:1.20.1+build.2")
+    modImplementation("net.fabricmc:fabric-loader:0.14.21")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.83.1+1.20.1")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.9.5+kotlin.1.8.22")
 
     modImplementation("net.silkmc:silk-core:$silkVersion")
 }
