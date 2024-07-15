5db05e4c444d1d83117418f9469b1f2640abbf8b
Krxwallo <59575572+Krxwallo@users.noreply.github.com>
2024-07-15 16:09:48 +0200

    mc 1.21, kotlin 2.0.0, gradle 8.9, silk 1.10.7 etc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
-val javaVersion = 21
-val silkVersion = "1.10.5"
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+val javaVersion = JvmTarget.JVM_21
+val silkVersion = "1.10.7"
 
 plugins {
-    kotlin("jvm") version "1.9.23"
-    id("fabric-loom") version "1.6-SNAPSHOT"
+    kotlin("jvm") version "2.0.0"
+    id("fabric-loom") version "1.7-SNAPSHOT"
 }
 
 group = "org.example"
@@ -14,10 +16,10 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.6")
-    mappings("net.fabricmc:yarn:1.20.6+build.1")
-    modImplementation("net.fabricmc:fabric-loader:0.15.10")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:0.97.8+1.20.6")
+    minecraft("com.mojang:minecraft:1.21")
+    mappings("net.fabricmc:yarn:1.21+build.9")
+    modImplementation("net.fabricmc:fabric-loader:0.16.0")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.100.7+1.21")
     modImplementation("net.fabricmc:fabric-language-kotlin:1.10.19+kotlin.1.9.23")
 
     modImplementation("net.silkmc:silk-core:$silkVersion")
@@ -25,14 +27,14 @@ dependencies {
 
 tasks {
     compileKotlin {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjdk-release=$javaVersion", "-Xskip-prerelease-check")
-            jvmTarget = "$javaVersion"
+        compilerOptions {
+            freeCompilerArgs = listOf("-Xjdk-release=${javaVersion.target}", "-Xskip-prerelease-check")
+            jvmTarget.set(javaVersion)
         }
     }
     compileJava {
         options.encoding = "UTF-8"
-        options.release.set(javaVersion)
+        options.release.set(javaVersion.target.toInt())
     }
     processResources {
         val properties = mapOf("version" to project.version)
